1838fe7bf73106b699da98c7e7f4e2b5fec5ac77
Xi LIU <xi.liu@u.nus.edu>
2026-01-26 14:44:01 +0800

    load

--- run_plot_band.py
+++ run_plot_band.py
@@ -5,7 +5,7 @@ import sys
 from classes.class_defs import frac_to_cartesian, atomIndex, hopping, vertex, T_tilde_total
 #loading Hk module
 
-from load_Hk import load_hamiltonian_from_path
+from load_Hk_parameters.load_Hk_and_hopping import load_hamiltonian_and_hopping_from_path
 
 
 argErrCode = 20
@@ -17,4 +17,7 @@ if (len(sys.argv) != 2):
 
 confFileName = str(sys.argv[1])
 
-hamiltonian_data=load_hamiltonian_from_path(confFileName,True)
+hamiltonian_data=load_hamiltonian_and_hopping_from_path(confFileName,True)
+
+
+print(hamiltonian_data["hopping_parameters"])
